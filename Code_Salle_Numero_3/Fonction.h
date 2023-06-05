a04aed88f91b6b207a342f8fe6df4c8b718eea89
GitDimi <135097203+GitDimi@users.noreply.github.com>
2023-06-05 13:08:01 +0200

    Update Fonction.h

--- Code_Salle_Numero_3/Fonction.h
+++ Code_Salle_Numero_3/Fonction.h
@@ -10,45 +10,29 @@ Toute la logique est dans ses fonctions.
 On gère aussi les différant mode choisi dans Parametre.h
 */
 
-bool Old_State_Capteur01 = 0;
-bool Old_State_Capteur02 = 0;
+//Init Timer
 unsigned long Timer_Temps_Detection_max = 0;
-bool Mouvement = 0;
-
-unsigned long Timer_Delay_Lumiere = 0;
-unsigned long Timer_Temps_Non_Lumiere = 0;
-bool Old_State_Timer_Delay_Lumiere = 0;
-bool Old_State_Timer_Temps_Lumiere = 0;
-unsigned long Timer_Temps_Lumiere_Up = 0;
-unsigned long Timer_Temps_Lumiere_Down = 0;
-int Lumiere_Down = 0;
-int Lumiere_Up = 0;
-
+unsigned long Timer_Delay_Diffuseur = 0;
+unsigned long Timer_Temps_Diffuseur = 0;
+unsigned long Timer_Delay_Rediffuseur = 0;
+unsigned long Timer_Temps_Rediffuseur = 0;
 unsigned long Timer_Delay_Musique = 0;
-bool Old_State_Timer_Delay_Musique = 0;
-unsigned long Timer_Ajuste_Musique = 0;
-unsigned long Timer_Stop_Musique = 0;
-
-unsigned long Timer_Delay_Souffle = 0;
+unsigned long Timer_Delay_Ventilateur = 0;
 unsigned long Timer_Temps_Souffle = 0;
 unsigned long Timer_Temps_Non_Souffle = 0;
+unsigned long Timer_Delay_Souffle = 0;
+
+//Init lancement timer
+bool Old_State_Diffusion = 0;
+bool Old_State_No_Mouvement = 0;
+bool Old_State_Timer_Delay_Musique = 0;
+bool Old_State_Timer_Delay_Ventilateur = 0;
 bool Old_State_Timer_Delay_Souffle = 0;
 bool Old_State_Timer_Temps_Souffle = 0;
 
-unsigned long Timer_Delay_Diffuseur = 0;
-unsigned long Timer_Temps_Diffuseur = 0;
-unsigned long Timer_Temps_Non_Diffuseur = 0;
-bool Old_State_Timer_Delay_Diffuseur = 0;
-bool Old_State_Timer_Temps_Diffuseur = 0;
-
-unsigned long Timer_Delay_Ventilateur = 0;
-unsigned long Timer_Temps_Ventilateur = 0;
-unsigned long Timer_Temps_Non_Ventilateur = 0;
-bool Old_State_Timer_Delay_Ventilateur = 0;
-bool Old_State_Timer_Temps_Ventilateur = 0;
-
-unsigned long Timer_Delay_Deshumidificateur = 0;
-bool Old_State_Timer_Delay_Deshumidificateur = 0;
+//Init Variable
+bool Mouvement = 0;
+int Compteur_Souffle = 0;
 
 //Fonction du fichier Fonction.ino
 
@@ -61,22 +45,15 @@ bool Old_State_Timer_Delay_Deshumidificateur = 0;
   */
   void Gestion_Capteur();
   /*
-  Cette fonction gère la lumière.
-  Grace à des paramètres dans parametre.h on peut choisi : 
-  - l'intensité de la lumière quand elle est forte
-  - Le temps que la lumière forte est active
-  - l'intensité de la lumière quand elle est faible
-  - Le temps que la lumière faible est active
+  Cette fonction gère une led
+  La led s'allume si le programme considère qu'une personne est détecter dans la pièce. Et inversement.
   */
-  void Gestion_Lumiere();
+  void Gestion_LED();
   /*
   Cette fonction gère la musique (bruit de respiration).
   Une fois une personne détectée on lance la musique après avoir respecté le temps de delay prévu dans parametre.h.
-  Plusieurs modes sont possible : 
-  mode 1 : 12 s d'inspiration et 8 s d'expiration
-  mode 2 : 9 s d'inspiration et 6 s d'expiration
-  mode 3 : Respiration humaine
-  Ses mode sont gérable dans parametre.h.
+  On gère aussi si l'on stop la musique tout de suite après la sortie du visiteur ou l'on finit la lecture de la tracks.
+  On gère aussi si l'on répète la musique une fois la musique fini (Si une personne est toujours à l'intérieur).
   */
   void Gestion_Musique();
   /*
@@ -84,31 +61,34 @@ bool Old_State_Timer_Delay_Deshumidificateur = 0;
   Quand une personne est détectée on lance le souffle après un delay prévu dans parametre.h.
   Le souffle est fait d'une phase d'expiration et d'une phase d'inspiration.
   Ses deux phases peuvent être contrôlé dans le temps par des paramètre dans parametre.h.
+  L'expiration se fait de manière linaire. Elle commence fort puis s'affaibli.
   */
   void Gestion_Souffle();
   /*
-  Cette fonction gère la diffusion.
-  Grace à un paramètre dans parametre.h on peut choisi le temps que le diffuseur est actif
-  et le temps que le diffuseur est inactif
-  */
-  void Gestion_Diffusion();
-  /*
-  Cette fonction gère le ventilateur.
-  Grace à un paramètre dans parametre.h on peut choisi le temps que le ventilateur est actif
-  et le temps que le ventilateur est inactif
+  Cette fonction gère la ventilation 
+  Quand personne n'est détecté on lance la ventilation de la salle après un delay prévu dans parametre.h.
+  Quand une personne est détectée on arrête immédiatement la ventilation.
   */
   void Gestion_Ventilateur();
   /*
-  Cette fonction gère la deshumidification
-  Quand personne n'est détecté on lance la deshumidification de la salle après un delay prévu dans parametre.h.
-  Quand une personne est détectée on arrête immédiatement la deshumidification.
+  Cette fonction gère la diffusion.
+  Cette fonction gère les deux modes de diffusion possible en fonction d'un paramètre dans parametre.h.
   */
-  void Gestion_Deshumidificateur();
+  void Gestion_Diffusion();
   /*
-  Cette fonction gère une led
-  La led s'allume si le programme considère qu'une personne est détecter dans la pièce. Et inversement.
+  Cette fonction gère la partie diffusion en mode normal
+  Cette fonction ouvre le diffuseur quand une personne arrive après un certain delay donné dans parametre.h.
+  Le diffuseur sera ouvert le temps donné dans parametre.h. une fois ce temps passé le diffuseur s'arrête.
   */
-  void Gestion_LED();
+  void Gestion_Diffusion_Normal();
+  /*
+  Cette fonction gère la partie diffusion en mode rediffusion
+  Cette fonction ouvre le diffuseur quand une personne arrive après un certain delay donné dans parametre.h.
+  Le diffuseur sera ouvert le temps donné dans parametre.h. une fois ce temps passé le diffuseur s'arrête.
+  Le diffuseur se réouvre ensuite à l'intervalle régulier qu'un paramètre dans parametre.h règle.
+  On peut également choisir le temps de réouverture à l'aide d'un paramètre.
+  */
+  void Gestion_Rediffusion();
 
 #endif 
 
