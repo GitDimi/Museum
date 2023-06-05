38c06d600888403a05a2e3f4974d68710c6dc918
GitDimi <135097203+GitDimi@users.noreply.github.com>
2023-06-05 13:09:26 +0200

    Update Init.ino

--- Code_Salle_Numero_3/Init.ino
+++ Code_Salle_Numero_3/Init.ino
@@ -7,66 +7,45 @@ void Init()
   pinMode(Capteur_de_mouvement_02, INPUT);
 
   //OUTPUT
-  pinMode(Pin_Lumiere, OUTPUT);  
-  pinMode(Pin_Musique1, OUTPUT);
-  pinMode(Pin_Musique2, OUTPUT);
-  pinMode(Pin_Musique3, OUTPUT);
-  pinMode(Pin_Diffuseur,OUTPUT);
-  pinMode(Pin_Souffle, OUTPUT);
-  pinMode(Pin_Ventilateur, OUTPUT);  
-  pinMode(Pin_Deshumidificateur, OUTPUT);  
+  pinMode(Pin_Diffuseur, OUTPUT);
+  pinMode(Pin_Musique, OUTPUT);
+  pinMode(Pin_Alim_Musique, OUTPUT);
+  pinMode(Pin_Ventilateur, OUTPUT);
   pinMode(Pin_LED, OUTPUT);
+  pinMode(Pin_Souffle, OUTPUT);
   
   //Etat des pins au démarrage 
-  analogWrite(Pin_Lumiere,0);
-  digitalWrite(Pin_Musique1, HIGH);
-  digitalWrite(Pin_Musique2, HIGH);
-  digitalWrite(Pin_Musique3, HIGH);
+  digitalWrite(Pin_Musique, HIGH);
+  digitalWrite(Pin_Alim_Musique, HIGH);
   digitalWrite(Pin_Diffuseur, LOW);
-  analogWrite(Pin_Souffle,0);
   digitalWrite(Pin_Ventilateur, LOW);
-  digitalWrite(Pin_Deshumidificateur, LOW);
   digitalWrite(Pin_LED, LOW);
-
-  //Permet de communiqué sur le port série a 9600 bauds
-  Serial.begin(9600);
-
-  Serial.println("Attente de mise a 0 des capteurs");
-
-  while(digitalRead(Capteur_de_mouvement_02) == 1 || digitalRead(Capteur_de_mouvement_01) == 1)
-  {
-    Serial.println("Catpeur01");
-    Serial.println(digitalRead(Capteur_de_mouvement_02));
-    Serial.println("Catpeur02");
-    Serial.println(digitalRead(Capteur_de_mouvement_02));
-  } 
-  
-  Serial.println("Capteurs prêt");
-
-  //Pour eviter que les timers perenne comme 1ère valeur 0 dans le millis();
-  delay(1);
+  analogWrite(Pin_Souffle,0);
 }
 
 void Timing_Warning()
-{ 
+{
+  //Permet de communiqué sur le port série a 9600 bauds
+  Serial.begin(9600);
+  
   //Verification des timing
-  if(Intensitie_Lumiere_Forte > 255 || Intensitie_Lumiere_Forte < 0)
+  if(Rediffusion)
   {
-     Serial.println("Valeur de Intensitie_Lumiere_Fort problèmatique");
-  }
- 
-  if(Intensitie_Lumiere_Faible > 255 || Intensitie_Lumiere_Forte < 0)
-  {
-     Serial.println("Valeur de Intensitie_Lumiere_Faible problèmatique");
-  }
-
-  if(Vitesse_Musique > 3 || Vitesse_Musique <= 0)
-  {
-    Serial.println("Valeur de Vitesse_Musique problèmatique");
-  }
-
-  if(Detecteur_1 == 0 && Detecteur_2 == 0)
-  {
-    Serial.println("Aucun capteur activé");
+    if(Delay_Rediffuseur < Temps_Rediffuseur)
+    {
+      Serial.println("Attention Delay_Rediffuseur < Temps_Rediffuseur");
+    }
+    if(Delay_Rediffuseur < Temps_Diffuseur)
+    {
+      Serial.println("Attention Delay_Rediffuseur < Temps_Diffuseur");
+    }
+    if(Delay_Rediffuseur < Delay_Diffuseur)
+    {
+      Serial.println("Attention Delay_Rediffuseur < Delay_Diffuseur");
+    }
+    if(Delay_Rediffuseur > Temps_Detection_max)
+    {
+      Serial.println("Attention Delay_Rediffuseur > Temps_Detection_max");
+    }
   }
 }
