75eaf870f675280305920ca5e70d23a49293ca01
GitDimi <135097203+GitDimi@users.noreply.github.com>
2023-06-06 09:37:56 +0200

    Add files via upload

---
+++ Code_Salle_Numero_3/Calcule.h
@@ -0,0 +1,30 @@
+/*
+SOUFFLE
+*/
+//Combien de temps dure une expiration (Vapeur allumé)
+#define Temps_Souffle Temps_Expiration
+//Combien de temps dure une inspiration (Vapeur éteint)
+#define Temps_Non_Souffle Temps_Inspiration
+
+/*
+DIFFUSEUR
+*/
+//Combien de temps dure une diffusion (Diffuseur allumé)
+#define Temps_Diffuseur Temps_Expiration
+//Combien de temps dure la pause de diffusion (Diffuseur éteint)
+#define Temps_Non_Diffuseur Temps_Inspiration
+
+/*
+VENTILATEUR
+*/
+//Combien de temps dure une ventilation (Ventilateur allumé)
+#define Temps_Ventilateur Temps_Expiration
+//Combien de temps dure la pause de ventilation (Ventilateur éteint)
+#define Temps_Non_Ventilateur Temps_Inspiration
+
+/*
+LUMIERE
+*/
+#define Temps_Lumiere_Forte Temps_Expiration
+//Combien de temps allume t-on la lumière en mode faible 
+#define Temps_Lumiere_Faible Temps_Inspiration
