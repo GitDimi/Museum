a2f83aecf6cf097c1acce53b6b4c93340f29e455
GitDimi <135097203+GitDimi@users.noreply.github.com>
2023-06-06 09:36:36 +0200

    Update Hardware.h

--- Code_Salle_Numero_3/Hardware.h
+++ Code_Salle_Numero_3/Hardware.h
@@ -1,8 +1,6 @@
 #ifndef Hardware_h
 #define Hardware_h
 
-#include "Parametre.h"
-
 /* 
 DESCRIPTION 
 
@@ -17,12 +15,20 @@ Les périphériques sont activés ou désactiver en fonction des paramètres du 
   #define Capteur_de_mouvement_02 3
 
 //Pin de sortie
-  #define Pin_Diffuseur 13
-  #define Pin_Musique 12
-  #define Pin_Ventilateur 11
-  #define Pin_LED 10
-  #define Pin_Alim_Musique 9
-  #define Pin_Souffle 5
+
+  #define Pin_Musique1 4
+  #define Pin_Musique2 5
+  #define Pin_Musique3 6
+  #define Pin_LED 7
+  #define Pin_Moteur_Fermeture 8
+  #define Pin_Moteur_Ouverture 9
+  #define Pin_Ventilateur 10
+  #define Pin_Lumiere 11
+  #define Pin_Souffle 12
+  #define Pin_Deshumidificateur 13
+
+//Temps static
+  #define Temps_Moteur 730
 
 //Fonction du fichier Hardware.ino
   /*
@@ -36,25 +42,40 @@ Les périphériques sont activés ou désactiver en fonction des paramètres du 
   */
   bool Fonction_Catpeur02();
   /*
-  Allume ou éteint le diffuseur.
-  (bool) : La valeur qui va être donnée au diffuseur.
+  Gère l'intensité de la lumière
+  int : Valeur de l'intensité
   */
-  void Fonction_Diffuseur(bool);
-  /*
+  void Fonction_Lumiere(int);
+    /*
   Allume ou éteint la Musique. 
   (bool) : L'état qui va être donnée à la Musique.
   */
   void Fonction_Musique(bool);
   /*
-  Allume ou éteint le ventilateur. 
-  (bool) : L'état qui va être donnée au ventilateur.
+  Ferme le diffuseur.
+  (bool) : La valeur qui va être donnée au diffuseur.
+  */
+  void Fonction_Moteur_Fermeture(bool);
+  /*
+  Ouvre le diffuseur.
+  (bool) : La valeur qui va être donnée au diffuseur.
+  */
+  void Fonction_Moteur_Ouverture(bool);
+  /*
+  Allume ou éteint le souffle.
+  (Bool) : La valeur qui va être donnée au souffle.
+  */
+  void Fonction_Souffle(bool);
+  /*
+  Allume ou éteint le ventilateur.
+  (Bool) : La valeur qui va être donnée au ventilateur.
   */
   void Fonction_Ventilateur(bool);
   /*
-  Gère la force du souffle. 
-  (int) : La valeur qui va être donnée au souffle.
+  Allume ou éteint le Deshumidificateur. 
+  (bool) : L'état qui va être donnée au Deshumidificateur.
   */
-  void Fonction_Souffle(int);
+  void Fonction_Deshumidificateur(bool);
   /*
   Allume ou éteint la LED. 
   (bool) : L'état qui va être donnée à la LED.
