8f912066a0f746e0ad769d9850a7783cd02678dd
GitDimi <135097203+GitDimi@users.noreply.github.com>
2023-06-06 15:24:48 +0200

    Update Parametre.h

--- Code_Salle_Numero_3/Parametre.h
+++ Code_Salle_Numero_3/Parametre.h
@@ -1,5 +1,5 @@
 /*
-Activer ou désactiver les perifiériques
+Activer ou désactiver les périphériques
 1 = activé
 0 = désactivé
 */
@@ -22,28 +22,27 @@ Activer ou désactiver les perifiériques
 //Lumière
 #define Lumiere 1
 
-
 // -------------- GESTION DES CAPTEUR -------------------- //
 
 /*
 CAPTEUR
 */
-//Combien de temps on dit que quelqu'un est dans la piece après ne plus détécté personne 
+//Combien de temps on dit que quelqu'un est dans la pière après ne plus détecté personne 
 #define Temps_Detection_max 20000
 
 /*
 GENERAL
 */
-//De combien de temps sont les temps d'inpiration et expiration
+//De combien de temps sont les temps d'inspiration et expiration
 #define Temps_Inspiration 12000 
 #define Temps_Expiration 8000
 
 /*
 MUSIQUE
 */
-//Combien de temps se lance la musique après une detection 
+//Combien de temps se lance la musique après une détection 
 #define Delay_Musique 1000
-/*A quelle vitesse la repiration va ?
+/*A quelle vitesse la respiration va ?
 mode 1 : 12 s d'inspiration et 8 s d'expiration
 mode 2 : 9 s d'inspiration et 6 s d'expiration
 mode 3 : Respiration humaine
@@ -53,34 +52,35 @@ mode 3 : Respiration humaine
 /*
 SOUFFLE
 */
-//Combien de temps se lance le souffle après une detection 
+//Combien de temps se lance le souffle après une détection 
 #define Delay_Souffle 12000
 
 /*
 DIFFUSEUR
 */
-//Combien de temps se lance le diffuseur après une detection 
-#define Delay_Diffuseur 4000
+//Combien de temps se lance le diffuseur après une détection 
+#define Delay_Diffuseur 5500
 
 /*
 VENTILATEUR
 */
-//Combien de temps se lance le ventilateur après une detection 
-#define Delay_Ventilateur 5000
+//Combien de temps se lance le ventilateur après une détection 
+#define Delay_Ventilateur 6500
 
 /*
 LUMIERE
 */
-//Après combien de temps on lance le cycle de lumière après une detection 
+//Après combien de temps on lance le cycle de lumière après une détection 
 #define Delay_Lumiere 12000
 //Quelle est l'intensité de la lumière forte (de 0 a 255)
 #define Intensitie_Lumiere_Forte 60
-//Quelle est l'intensité de la lumière forte (de 0 a 255) (d'experiance 0 à 12 = éteint)
+//Quelle est l'intensité de la lumière forte (de 0 a 255) (d'expérience 0 à 12 = éteint)
 #define Intensitie_Lumiere_Faible 13
 
 /*
 DESHUMIDIFICATEUR
 */
-//Combien de temps se lance la deshumidification après une detection 
-#define Delay_Deshumidificateur 20000
+//Combien de temps se lance la déshumidification après une détection 
+#define Delay_Deshumidificateur 30000
+
 
