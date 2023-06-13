4d00eb78554a545366b0ef334d54b1d4fd1a2c3b
GitDimi <135097203+GitDimi@users.noreply.github.com>
2023-06-13 11:00:21 +0200

    Update Fonction.h

--- Code_Salle_Numero_3/Fonction.h
+++ Code_Salle_Numero_3/Fonction.h
@@ -5,35 +5,45 @@
 DESCRIPTION 
 
 Les fichiers Fonction gèrent le gros du programme.
-C'est ici que l'on va pour chaque périphérique choisir quand lire, allumer, éteindre, ext... 
-Toute la logique est dans ses fonctions.
-On gère aussi les différant mode choisi dans Parametre.h
+C'est ici que l'on va, pour chaque périphérique, choisir quand lire, allumer, éteindre, ext... 
+Toute la logique est donc ici.
 */
 
+//Déclaration des variables de Fonction.ino
+
+//Capteur
 bool Old_State_Capteur01 = 0;
 bool Old_State_Capteur02 = 0;
 unsigned long Timer_Temps_Detection_max = 0;
 bool Mouvement = 0;
 
+//Lumière
 bool Old_State_Timer_Delay_Lumiere = 0;
 unsigned long Timer_Delay_Lumiere = 0;
-unsigned long Timer_Temps_Lumiere = 0;
-unsigned long Timer_Temps_Non_Lumiere = 0;
+unsigned long Timer_Temps_Lumiere_Up_Save = 0;
+unsigned long Timer_Temps_Lumiere_Down_Save = 0;
 unsigned long Timer_Temps_Lumiere_Up = 0;
 unsigned long Timer_Temps_Lumiere_Down = 0;
+unsigned long Timer_Temps_Lumiere_Pause = 0;
+unsigned long Timer_Temps_Lumiere_DMX = 0;
 int Lumiere_Down = 0;
 int Lumiere_Up = 0;
 
+//Musique
 bool Old_State_Timer_Delay_Musique = 0;
 unsigned long Timer_Delay_Musique = 0;
 unsigned long Timer_Ajuste_Musique = 0;
 unsigned long Timer_Stop_Musique = 0;
+int Nb_Musique = 0;
 
+//Souffle
 bool Old_State_Timer_Delay_Souffle = 0;
 unsigned long Timer_Delay_Souffle = 0;
 unsigned long Timer_Temps_Souffle = 0;
 unsigned long Timer_Temps_Non_Souffle = 0;
+unsigned long Timer_Security_Souffle = 0;
 
+//Diffuseur
 bool Old_State_Timer_Delay_Diffuseur = 0;
 unsigned long Timer_Delay_Diffuseur = 0;
 unsigned long Timer_Temps_Diffuseur = 0;
@@ -43,11 +53,15 @@ bool Old_State_Security_Diffuseur = 0;
 int State_Diffuseur = 2;
 int Moteur_Security = 0;
 
+//ventilateur
 bool Old_State_Timer_Delay_Ventilateur = 0;
 unsigned long Timer_Delay_Ventilateur = 0;
 unsigned long Timer_Temps_Ventilateur = 0;
 unsigned long Timer_Temps_Non_Ventilateur = 0;
+unsigned long Timer_Vitesse_Ventilateur_Up = 0;
+unsigned long Timer_Vitesse_Ventilateur_Down = 0;
 
+//deshumidifiacteur 
 bool Old_State_Timer_Delay_Deshumidificateur = 0;
 unsigned long Timer_Delay_Deshumidificateur = 0;
 
@@ -56,9 +70,9 @@ unsigned long Timer_Delay_Deshumidificateur = 0;
   /*
   Cette fonction gère les capteurs.
   Elle est programmée pour que tant qu'il ait un des deux capteurs qui perçois un mouvement, Le programme considère qu'il ait une personne dans la pièce.
-  Dans le cas où les capteurs ne détectent plus personne du a l'immobilité de cette dernière, 
+  Dans le cas où les capteurs ne détectent plus personne du a l'absence de cette dernière, 
   un paramètre dans Paramere.h permet de prolonger l'état ou le programme considère qu'il y ai une personne dans la pièce.
-  Si aucun capteur ne détecte un mouvement et que le delay de prolongation est terminer les valeurs, le programme indiquera que la pièce est vide.
+  Si aucun capteur ne détecte un mouvement et que le delay de prolongation est terminer, le programme indiquera que la pièce est vide.
   */
   void Gestion_Capteur();
   /*
@@ -66,38 +80,48 @@ unsigned long Timer_Delay_Deshumidificateur = 0;
   Grace à des paramètres dans parametre.h on peut choisi : 
   - l'intensité de la lumière quand elle est forte
   - l'intensité de la lumière quand elle est faible
+  - Le temps que la lumière passe de faible à fort 
+  - Le temps que la lumière passe de fort à faible
+  - Le temps que la lumière reste faible avant de repartir
+  - Le delay pour que la lumière s'allume par rapport au début du souffle
   */
   void Gestion_Lumiere();
   /*
   Cette fonction gère la musique (bruit de respiration).
-  Une fois une personne détectée on lance la musique après avoir respecté le temps de delay prévu dans parametre.h.
-  Plusieurs modes sont possibles : 
-  mode 1 : 12 s d'inspiration et 8 s d'expiration
-  mode 2 : 9 s d'inspiration et 6 s d'expiration
-  mode 3 : Respiration humaine
-  Ses modes sont gérables dans parametre.h.
+  Grace à un paramètre dans parametre.h on peut choisi : 
+  - Le delay pour que la musique s’enclenche par rapport au début du souffle
+  La musique possède 4 tracks qui tourne l'une après les autres.
   */
   void Gestion_Musique();
   /*
-  Cette fonction gère le souffle (souffle du dinosaure).
-  Quand une personne est détectée on lance le souffle après un delay prévu dans parametre.h.
-  Le souffle est fait d'une phase d'expiration et d'une phase d'inspiration.
+  Cette fonction gère le souffle (souffle du dinosaure produit par une projection de vapeur d'eau).
+  Grace à des paramètres dans parametre.h on peut choisi : 
+  - Le delay pour que le souffle se lance par rapport à la détection d'une personne
+  - Le temps que le souffle est actif
+  Le souffle est fait d'une phase d'expiration (vapeur d'eau) et d'une phase d'inspiration (calme).
   */
   void Gestion_Souffle();
   /*
-  Cette fonction gère la diffusion.
-  On lance la diffusion après un delay prévu dans parametre.h.
+  Cette fonction gère la diffusion (ouverture du contenant de l'odeur).
+  Grace à des paramètres dans parametre.h on peut choisi : 
+  - Le delay pour que la diffusion s’enclenche par rapport au début du souffle
+  - Le temps que la diffusion est active
+  La diffusion est faite d'une phase d'ouverture (contenant ouvert) et d'une phase de fermeture (contenant fermé).
   */
   void Gestion_Diffusion();
   /*
-  Cette fonction gère le ventilateur.
-  On lance le ventilateur après un delay prévu dans parametre.h.
+  Cette fonction gère le ventilateur (le ventilateur qui sort l'aire du contenant de du diffuseur et les ventilateurs qui projettent l'aire dans la pièce).
+  Grace à des paramètres dans parametre.h on peut choisi : 
+  - Le delay pour que les ventilateurs s’enclenchent par rapport au début du souffle
+  - Le temps que les ventilateurs sont activés
+  La ventilation est faite d'une phase de fonctionnement (ventilateur ON) et d'une phase d'arrêt (ventilateur OFF).
   */
   void Gestion_Ventilateur();
   /*
-  Cette fonction gère la déshumidification
-  Quand personne n'est détecté on lance la déshumidification de la salle après un delay prévu dans parametre.h.
-  Quand une personne est détectée on arrête immédiatement la deshumidification.
+  Cette fonction gère la déshumidification.
+  Grace à des paramètres dans parametre.h on peut choisi : 
+  - Le delay pour que les déshumidifications s’enclenchent par rapport à la fin de la détection d'une personne
+  Même une fois la prise de l'alimentation du Arduino retiré, le déshumidificateur fonctionne toujours pour qu'il soit active la nuit
   */
   void Gestion_Deshumidificateur();
   /*
